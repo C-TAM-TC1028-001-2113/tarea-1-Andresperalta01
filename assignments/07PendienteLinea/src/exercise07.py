5937d35c4a51cae3a36ff3f4a6b3c7250c212b0e
Andresperalta01 <89591759+Andresperalta01@users.noreply.github.com>
2021-08-30 10:20:04 -0500

    Add files via upload

---
+++ assignments/07PendienteLinea/src/exercise07.py
@@ -0,0 +1,12 @@
+def main():
+    # escribe tu codigo abajo de esta linea
+    x1 = float(input())
+    y1 = float(input())
+    x2 = float(input())
+    y2 = float(input())
+    m = (y2-y1)/(x2-x1)
+    print('la pendiente es:', m)
+
+
+if __name__ == '__main__':
+    main()
