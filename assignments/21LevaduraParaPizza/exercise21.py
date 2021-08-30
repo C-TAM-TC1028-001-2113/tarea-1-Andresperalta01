ea10c80ce860bf67e2662b2d8829322f6274b767
Andresperalta01 <89591759+Andresperalta01@users.noreply.github.com>
2021-08-30 10:28:28 -0500

    Add files via upload

---
+++ assignments/21LevaduraParaPizza/exercise21.py
@@ -0,0 +1,9 @@
+def main():
+    # escribe tu código abajo de esta línea
+    g_harina = float(input('dame la harina en gramos'))
+    calc = (g_harina) * (0.050)
+    calc = round(calculo, 1)
+    print('necesitas estos gramos de lavadura:', calculo)
+
+if __name__ == '__main__':
+    main()
